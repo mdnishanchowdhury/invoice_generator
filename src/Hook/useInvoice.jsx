bebe7750e2c43e9d48d24488faa7be6bc5b7fc52
Md Nishan Chowdhury <mdnishanchowdhury.cse@gmail.com>
2025-10-16 00:40:39 +0600

    add backend folder

--- src/Hook/useInvoice.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { InvoiceContext } from "../context/InvoiceProvider";
-
-function useInvoice() {
-    const invoice = useContext(InvoiceContext);
-    return invoice;
-}
-
-export default useInvoice;
