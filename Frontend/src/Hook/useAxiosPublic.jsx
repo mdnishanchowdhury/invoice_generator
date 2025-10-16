db38971180923a76bbdf1c3fe902aee6891c1236
Md Nishan Chowdhury <mdnishanchowdhury.cse@gmail.com>
2025-10-16 22:58:43 +0600

    All Page optimized

--- Frontend/src/Hook/useAxiosPublic.jsx
+++ Frontend/src/Hook/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    // baseURL: 'https://backend-bay-seven-52.vercel.app'
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://backend-bay-seven-52.vercel.app'
+    // baseURL: 'http://localhost:5000'
 });
 const useAxiosPublic = () => {
     return axiosPublic;
